9a3b7221328c442963333e409873488853025731
Henry Ifebunandu <henryifebunandu@gmail.com>
2021-03-01 23:57:30 +0100

    removed many unnecessary files, widget now updates after 30minsgit add --all!

--- app/src/main/java/com/example/wrapfluttermodule/StackWidgetProvider.java
+++ app/src/main/java/com/example/wrapfluttermodule/StackWidgetProvider.java
@@ -53,31 +53,32 @@ public class StackWidgetProvider extends AppWidgetProvider {
             // When intents are compared, the extras are ignored, so we need to embed the extras
             // into the data so that the extras will not be ignored.
             intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+            intent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
+
             RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.listview_layout);
             rv.setRemoteAdapter(R.id.listviewshow, intent);
             // The empty view is displayed when the collection has no items. It should be a sibling
             // of the collection view.
-            rv.setEmptyView(R.id.listviewshow, R.id.empty_view);
+            rv.setEmptyView(R.id.listviewshow, R.id.empty_listview);
+
+            Intent toastIntent = new Intent(context, StackWidgetProvider.class);
+            toastIntent.setAction(StackWidgetProvider.TOAST_ACTION);
+            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+
+
+
             // Here we setup the a pending intent template. Individuals items of a collection
             // cannot setup their own pending intents, instead, the collection as a whole can
             // setup a pending intent template, and the individual items can set a fillInIntent
             // to create unique before on an item to item basis.
-            Intent toastIntent = new Intent(context, StackWidgetProvider.class);
-            toastIntent.setAction(StackWidgetProvider.TOAST_ACTION);
-            toastIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
-            intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
             PendingIntent toastPendingIntent = PendingIntent.getBroadcast(context, 0, toastIntent,
                     PendingIntent.FLAG_UPDATE_CURRENT);
             rv.setPendingIntentTemplate(R.id.listviewshow, toastPendingIntent);
             appWidgetManager.updateAppWidget(appWidgetIds[i], rv);
 
-//            Intent updateIntent = new Intent(context, UpdateService.class);
-//            updateIntent.setAction(AppWidgetManager.ACTION_APPWIDGET_UPDATE);
-//
-//            UpdateService.enqueueWork(context, updateIntent);
         }
 //        This commented line is supposedly going to update data on the stackviews
-//        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.listviewshow);
+        appWidgetManager.notifyAppWidgetViewDataChanged(appWidgetIds, R.id.listviewshow);
         super.onUpdate(context, appWidgetManager, appWidgetIds);
         Log.d(TAG, "OnUpdate StackWidgetProvider just ran");
     }
